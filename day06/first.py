5aa94573ef8823fc8a44f0929094b8f49b748f62
Antonio Martinović <martinovic.antonio@gmail.com>
2018-12-10 21:20:56 +0100

    Six, one

---
+++ day06/first.py
@@ -0,0 +1,37 @@
+import re
+import operator
+
+class SaneDict(dict):
+    def __missing__(self, key):
+        return 0
+
+
+def distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+r = re.compile('\d+')
+coords = [ list(map(int, r.findall(line))) for line in open("input.txt").readlines() ]
+max_x = max(map(lambda x: x[0], coords))
+max_y = max(map(lambda x: x[1], coords))
+coords = [ (i, coord) for i, coord in enumerate(coords) ]
+
+area = [ ['-'] * (max_y + 1) ] * ( max_x + 1)
+point_areas = SaneDict()
+infinite_areas = set()
+
+for x in range(max_x + 1):
+    for y in range(max_y + 1):
+        distances = sorted([ (i, distance(x, y, *p)) for i, p in coords ], key=operator.itemgetter(1))
+        if distances[0][1] == distances[1][1] or len(distances) == 1:
+            area[x][y] = '.'
+        else:
+            area[x][y] = distances[0][0]
+            point_areas[distances[0][0]] += 1
+    
+        if x == 0 or x == max_x or y == 0 or y == max_y:
+            infinite_areas.add(distances[0][0])
+
+for a in infinite_areas:
+    point_areas.pop(a)
+
+print(max(point_areas.items(), key=operator.itemgetter(1))[1])
---
+++ day06/readme.md
@@ -0,0 +1,10 @@
+### Day 6: Chronal Coordinates
+
+#### Part one
+At first I wanted to figure out a way to do this quickly. My idea was to go from
+point outwards until it reached a coordinate has on it a signature of another
+coordinate with the same or equal distance to it, and then to stop calculating
+for that point. 
+But I got too lazy to implement such a solution so I went for the slow and dumb.
+
+#### Part two
